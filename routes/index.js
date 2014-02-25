4914d042bd9516a502b9b19267b4b7ab3d75e6cd
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-24 17:30:25 -0800

    Add meta tags and fully integrate Foundation

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Triumphant Tasks',
-    scripts: ['bower_components/jquery/dist/jquery.min.js',
-              'bower_components/modernizr/modernizr.js',
+    scripts: ['bower_components/modernizr/modernizr.js',
+              'bower_components/jquery/dist/jquery.min.js',
               'bower_components/fastclick/lib/fastclick.js',
               'bower_components/foundation/js/foundation.min.js',
               'javascripts/driver.js']
