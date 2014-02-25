1230aea367fc04f8243945d17ea5fa936cffb518
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-24 17:04:55 -0800

    Integrate Foundation, Libsass in Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -14,6 +15,30 @@ module.exports = function(grunt) {
         dest: 'build/<%= pkg.name %>.min.js'
       }
     },
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/foundation/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'public/stylesheets/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+
     jshint: {
       all: [
         'gruntfile.js',
@@ -24,6 +49,7 @@ module.exports = function(grunt) {
         jshintrc: '.jshintrc'
       }
     },
+
     jasmine: {
       src: 'javascripts/client/**/*.js',
       options: {
@@ -31,6 +57,7 @@ module.exports = function(grunt) {
         helpers: 'spec/*Helper.js'
       }
     },
+
     vows : {
       all : {
         src : "spec/server/**/*.js",
@@ -44,8 +71,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-vows-runner');
 
+  grunt.registerTask('build', ['sass']);
   grunt.registerTask('test', ['jshint', 'jasmine']);
-  grunt.registerTask('default', ['uglify', 'test']);
+  grunt.registerTask('default', ['build', 'watch', 'uglify', 'test']);
 
 };
