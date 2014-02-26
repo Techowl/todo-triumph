367aec3b3fa288b53f2cb63f52bfbdf10f9edeaf
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-26 14:50:44 -0800

    Add Foundation navbar to layout Jade file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 exports.index = function(req, res){
-  res.render('index', { title: 'Triumphant Tasks',
+  res.render('index', {
     scriptsHead: ['bower_components/modernizr/modernizr.js'],
     scriptsBody: ['bower_components/jquery/dist/jquery.min.js',
                    'bower_components/fastclick/lib/fastclick.js',
